3e51e470768949b39cb62083270dbf0e7eb7b5c3
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-03-16 14:50:11 +0700

    Если маршрут вызван с методом GET, то просто отобразится форма регистрации. Если пришли данные от формы (метод POST), по произойдет попытка создать новую запись в таблице users и в представление отправится сообщение об успешной регистрации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,12 +4,14 @@ namespace Controller;
 
 use Model\Post;
 use Src\View;
+use Src\Request;
+use Model\User;
 
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $posts = Post::all();
+        $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
@@ -17,4 +19,13 @@ class Site
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
