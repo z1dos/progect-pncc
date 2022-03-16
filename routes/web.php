c3942cc3795bc0214a035819f8eccbd868be13e7
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-03-16 14:48:31 +0700

    вывод шаблона через адресную строку signup

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Src\Route;
 
 Route::add('posts', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
+Route::add('signup', [Controller\Site::class, 'signup']);
