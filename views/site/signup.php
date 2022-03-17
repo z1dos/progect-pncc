3ae3d49d12fc30ecfa61ee02c949a3c8b035eab3
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-03-17 18:16:03 +0700

    вывод формы регистрации

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,8 +1,13 @@
 <h2>Регистрация нового пользователя</h2>
-<h3><?= $message ?? ''; ?></h3>
+<h3><?= $messageAddBook ?? ''; ?></h3>
 <form method="post">
-    <label>Имя <input type="text" name="name"></label>
-    <label>Логин <input type="text" name="login"></label>
-    <label>Пароль <input type="password" name="password"></label>
+    <p>Фамилия <input type="text" name="surname"></p>
+    <p>Имя <input type="text" name="name"></p>
+    <p>Отчество <input type="text" name="patronymic"></p>
+    <p>Адрес <input type="text" name="address"></p>
+    <p>Номер телефона <input type="text" name="phone_number"></p>
+    <p>Читательский билет <input type="text" name="library_card"></p>
+    <p>Логин <input type="text" name="login"></p>
+    <p>Пароль <input type="password" name="password"></p>
     <button>Зарегистрироваться</button>
 </form>
