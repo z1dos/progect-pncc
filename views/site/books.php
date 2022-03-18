494a7340f12f79d604df7e75d34c70941b411eed
z1dos <georg.awdeew2017@yandex.ru>
2022-03-19 00:37:18 +0700

    Добавление стилей

--- views/site/books.php
+++ views/site/books.php
@@ -1,25 +1,42 @@
 <h1>Список книг</h1>
 <?php
 if (app()->auth::user()->isAdmin()):
-?>
-<h2><a href="<?= app()->route->getUrl('/addBooks') ?>">Добавить</a></h2>
+    ?>
+    <h2><a href="<?= app()->route->getUrl('/addBooks') ?>">Добавить</a></h2>
+<div class="view-books-all">
+            <?php
+            foreach ($books as $book) {
+                echo '<div class="view-books">' .
+                    '<p class="view-books-id">' . $book->id . '</p>' .
+                    '<div class="view-books-other">' .
+                    '<p>Название: ' . $book->title . '</p>' .
+                    '<p>Автор: ' . $book->author . '</p>' .
+                    '<p>Дата публикации: ' . $book->the_year_of_publishing . '</p>' .
+                    '<p>Издание: ' . $book->is_it_a_new_edition . '</p>' .
+                    '<p>Описание: ' . $book->description . '</p>' .
+                    '<p>Цена: ' . $book->price . '</p>' .
+                    '</div>' .
+                    '</div>';
+            }
+            ?>
+</div>
+<?php
+else:
+    ?>
     <ol>
         <?php
         foreach ($books as $book) {
-            echo '<li>' . $book->title . '</li>';
+            echo '<li>' .
+                '<p>Название: ' . $book->title . '</p>' .
+                '<p>Автор: ' . $book->author . '</p>' .
+                '<p>Дата публикации: ' . $book->the_year_of_publishing . '</p>' .
+                '<p>Издание: ' . $book->is_it_a_new_edition . '</p>' .
+                '<p>Описание: ' . $book->description . '</p>' .
+                '<p>Цена: ' . $book->price . '</p>' .
+                '</li>';
         }
         ?>
     </ol>
-<?php
-else:
-?>
-<ol>
-    <?php
-    foreach ($books as $book) {
-        echo '<li>' . $book->title . '</li>';
-    }
-    ?>
-</ol>
 <?php
 endif;
 ?>
