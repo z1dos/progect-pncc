5eae17681763928dadca79e794f959e092cb7010
z1dos <georg.awdeew2017@yandex.ru>
2022-03-18 12:17:15 +0700

    Вывод формы добалвения в чит карту

--- views/site/addInLibraryCard.php
+++ views/site/addInLibraryCard.php
@@ -1,1 +1,22 @@
-<?php
+<h2>Добавление в чититательский билет</h2>
+<h3><?= $message ?? ''; ?></h3>
+
+<form method="post">
+    <p>Номер читательского билета <select name="id_reader">
+            <?php
+            foreach ($users as $user ) {
+                echo '<option >' . $user->num_lybrary_card  . '</option >';
+            }
+            ?>
+        </select></p>
+    <p>ID книги <select name="id_book">
+            <?php
+            foreach ($books as $book ) {
+                echo '<option >' . $book->id  . '</option >';
+            }
+            ?>
+        </select></p>
+    <p>Дата выдачи <input type="date" name="date_of_receiving"></p>
+    <p>Дата сдачи <input type="date" name="delivery_date"></p>
+    <button>Добаить</button>
+</form>
