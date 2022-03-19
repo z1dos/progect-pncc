c9927f481897d71b322bd2056b0fea1ed0293c1e
z1dos <georg.awdeew2017@yandex.ru>
2022-03-19 22:42:47 +0700

    Добавление стилей

--- views/site/profile.php
+++ views/site/profile.php
@@ -1,23 +1,37 @@
 <h1>Профиль <?= app()->auth->user()->login ?? ''; ?></h1>
 <h2>Мой читательский билет</h2>
-<ol>
+<div class="view-showUsers-forAdmin">
+    <div>
+        <p>ID книги </p>
+        <p>Дата выдачи </p>
+        <p>Дата сдачи </p>
+        <p>Сдано </p>
+        <?php
+        if (app()->auth::user()->isAdmin()){
+            echo '<p>Поменять </p>';
+        }
+        ?>
+    </div>
+</div>
+<div class="view-profile">
     <?php
     foreach ($library_cards as $library_card) {
         echo
-            '<p>ID книги: ' . $library_card->id_book . '</p>' .
-            '<p>Дата выдачи: ' . $library_card->date_of_receiving . '</p>' .
-            '<p>Дата выдачи: ' . $library_card->delivery_date . '</p>' .
-            '<p>Сдано:' . $library_card->passed .'</p>';
-        if (app()->auth::user()->isAdmin()){
+            '<div>' .
+            '<p>' . $library_card->id_book . '</p>' .
+            '<p>' . $library_card->date_of_receiving . '</p>' .
+            '<p>' . $library_card->delivery_date . '</p>' .
+            '<p>' . $library_card->passed . '</p>';
+        if (app()->auth::user()->isAdmin()) {
             echo '<form method="post">' .
-            '<select>' .
-            "<option selected>$library_card->passed</option>".
-            '<option>Да</option> ' .
-            '</select>' .
+                '<select>' .
+                "<option selected>$library_card->passed</option>" .
+                '<option>Да</option> ' .
+                '</select>' .
                 '<button>Поменять</button>' .
-            '</form>';
+                '</form>' . '</div>';
         }
     }
-    echo '<h2>Пользователь: ' . $library_card->id_reader . '<h2>';
     ?>
+</div>
 </ol>
