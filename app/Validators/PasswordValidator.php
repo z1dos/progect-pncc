b6cf085cd660d5a6c645944ce4271a3b46317ed1
z1dos <georg.awdeew2017@yandex.ru>
2022-03-22 17:58:46 +0700

    Валидация пароля

--- app/Validators/PasswordValidator.php
+++ app/Validators/PasswordValidator.php
@@ -2,7 +2,15 @@
 
 namespace Validators;
 
-class PasswordValidator
-{
+use Src\Validator\AbstractValidator;
 
+class PasswordValidator extends AbstractValidator
+{
+    protected string $message = 'Field :field is login';
+
+    public function rule(): bool
+    {
+        $pN = $this->value;
+        return preg_match("/^[a-z0-9_-]{5,20}$/",$pN);
+    }
 }
