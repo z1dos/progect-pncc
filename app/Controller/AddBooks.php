770b388e8121e7293b6bcbcc2e3a821a0080cba1
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-03-22 11:01:55 +0700

    валидация добавления книги

--- app/Controller/AddBooks.php
+++ app/Controller/AddBooks.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Books;
 use Model\PublishingHouse;
 use Src\Request;
+use Src\Validator\Validator;
 use Src\View;
 
 class AddBooks
@@ -13,8 +14,29 @@ class AddBooks
     {
         $books = Books::all();
         $publishing_house = PublishingHouse::all();
-        if ($request->method === 'POST' && Books::create($request->all())) {
-            app()->route->redirect('/books');
+        if ($request->method === 'POST') {
+            $validator = new Validator($request->all(), [
+                'author' => ['required'],
+                'title' => ['required'],
+                'the_year_of_publishing' => ['required'],
+                'is_it_a_new_edition' => ['required'],
+                'description' => ['required'],
+                'price' => ['required'],
+            ], [
+                'required' => 'Поле :field пусто',
+            ]);
+
+            if($validator->fails()){
+                return new View('site.addBook',
+                    [
+                        'books' => $books,
+                        'publishing_house' => $publishing_house,
+                        'message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE),]);
+            }
+
+            if (Books::create($request->all())) {
+                app()->route->redirect('/books');
+            }
         }
         return (new View())->render('site.addBook', ['books' => $books, 'publishing_house' => $publishing_house]);
     }
