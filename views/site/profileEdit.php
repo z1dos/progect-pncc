9d82978a04dce53e9639b047eefd861a2651813b
z1dos <georg.awdeew2017@yandex.ru>
2022-03-23 22:51:57 +0700

    Корректировка

--- views/site/profileEdit.php
+++ views/site/profileEdit.php
@@ -1,4 +1,5 @@
 <h2>Редактирование</h2>
+<h3><?= $message ?? ''; ?></h3>
 <form method="post">
     <input name='csrf_token' type='hidden' value="<?= app()->auth::generateCSRF() ?>">
     <select name="passed" id="passed">
